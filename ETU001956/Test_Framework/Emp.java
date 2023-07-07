6e31c78b925428d3b81fcf4ec0ade74eb832b783
Nyaina03 <120169509+Nyaina03@users.noreply.github.com>
2023-07-07 14:50:54 +0300

    Create Emp.java

---
+++ ETU001956/Test_Framework/Emp.java
@@ -0,0 +1,11 @@
+package etu001956.test_framework;
+import etu001956.framework.annotation.Url;
+
+public class Emp {
+    @Url("/url3")
+    public void myMethod() {
+        // Code de la méthode
+    }
+}
+
+
